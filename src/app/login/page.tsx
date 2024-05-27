d981acfc3ff3a18edcefaa9c3d6ed2213023c7ee
Pegbeer <carlosgarciahl@hotmail.com>
2024-05-27 14:28:20 -0600

    Login screen Mobile

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import TextField from "@/app/components/TextField";
 import UserIcon from "/public/assets/vectors/person.svg";
 import LockIcon from "/public/assets/vectors/lock.svg"
+import GitHubIcon from "/public/assets/vectors/github-mark.svg"
+import GoogleIcon from "/public/assets/vectors/google.svg"
+import FilledButton from "../components/FilledButton";
 
 export default function LoginScreen() {
     return (
@@ -24,10 +27,23 @@ export default function LoginScreen() {
 
                 <div className="flex flex-row justify-between">
                     <label className="flex items-center space-x-2">
-                        <input type="checkbox" className="size-6 text-primary"/>
-                        <span className="text-lg text-gray-400">Opción</span>
+                        <input type="checkbox" className="size-6 text-primary checked:accent-primary"/>
+                        <span className="text-lg text-gray-400">Remember me</span>
                     </label>
+
+                    <a href="/public" className="text-gray-400">Forget password</a>
                 </div>
+
+                <FilledButton text="Login" backgroundColor="green" foregroundColor="white"/>
+
+                <div className="flex flex-row w-full items-center gap-4">
+                    <div className="border-t-2 border-gray-400 my-8 w-full"></div>
+                    <span className="text-gray-400">Or</span>
+                    <div className="border-t-2 border-gray-400 my-8 w-full"></div>
+                </div>
+                
+                <FilledButton text={"Login with GitHub"} backgroundColor={"white"} foregroundColor={"black"} icon={GitHubIcon} iconColor="accent"/>
+                <FilledButton text={"Login with Google"} backgroundColor={"white"} foregroundColor={"black"} icon={GoogleIcon} iconColor="accent"/>
             </div>
         </div>
     );
